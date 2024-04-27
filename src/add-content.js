9d49f2112b314dc9066f377e2cb6950d03cd79c3
tearknow <904916790@qq.com>
2024-04-27 11:33:01 +0800

    module.export

--- src/add-content.js
+++ src/add-content.js
@@ -1,3 +1,7 @@
-export default function (){
-    document.write("hello world - from webpack-dev-server!"+"<br>");
+
+module.exports = {
+    'firstFunc' : function(){
+        document.write("hello world - 来自模块导出中的方法"+"<br>");
+    },
+    'myName': 'jack is learning - 来自模块导出中的字符串',
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import myAddContent from './add-content.js'
+var moduleFunctions = require('./add-content.js') 
 
 document.write("My first webpack app 学习书本《webpack实战（入门进阶与调优）》 - by 居玉皓"+ "<br>");
 
-
-
-myAddContent();
+moduleFunctions.firstFunc();
+document.write(moduleFunctions.myName);
