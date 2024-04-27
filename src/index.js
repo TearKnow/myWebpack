6d7ad3d7cf3ee258b7c2418d95fbced2500c4cef
tearknow <904916790@qq.com>
2024-04-27 11:43:51 +0800

    es6 export

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 var moduleFunctions = require('./add-content.js') 
+import calculator from './calculator.js';
 
 document.write("My first webpack app 学习书本《webpack实战（入门进阶与调优）》 - by 居玉皓"+ "<br>");
 
 moduleFunctions.firstFunc();
 document.write(moduleFunctions.myName);
+
+
+
+document.write("<br>" + calculator.name + '- 来自es6 module的导出，和上面add-content.js那种有什么区别呢');
+document.write("<br>"+'2+3 = ' + calculator.add(2, 3) + '- 来自es6 module的导出方法');
