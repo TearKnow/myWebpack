638c5162a95b0a3af20b449068a79bcc084ae694
tearknow <904916790@qq.com>
2024-04-27 11:46:24 +0800

    change

--- src/index.js
+++ src/index.js
@@ -8,5 +8,5 @@ document.write(moduleFunctions.myName);
 
 
 
-document.write("<br>" + calculator.name + '- 来自es6 module的导出，和上面add-content.js那种有什么区别呢');
+document.write("<br>" + calculator.name + '- 来自es6 module的导出，和上面add-content.js那种有什么区别呢？add-content.js是使用Node.js的CommonJS模块导出语法，而这个是使用ES6模块导出语法');
 document.write("<br>"+'2+3 = ' + calculator.add(2, 3) + '- 来自es6 module的导出方法');
