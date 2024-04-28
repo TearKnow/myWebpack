5833e543651aadcb517752b3584edee78d93ae73
dunpai <dunpai@i9i8.com>
2024-04-28 14:05:51 +0800

    show how to use plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
     },
     mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "哈哈，我是title",
+            filename: "app.html",
+            template: "./src/html/index.html"
+        })
+    ],
     devServer: {
         static: '.',
     }
